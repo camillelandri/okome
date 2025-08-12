32c155c5b31f879d3aa9aa40055379a957194d87
camillelandri <camille.landri@gmail.com>
2025-08-12 15:29:45 +0200

    add sddm theme

--- .config/hypr/hypr/scripts/backup.sh
+++ .config/hypr/hypr/scripts/backup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-#sync folder 
-rsync -r ~/.config/fastfetch ~/okome/.config/fastfetch
-rsync -r ~/.config/gtk-3.0 ~/okome/.config/gtk-3.0
-rsync -r ~/.config/gtk-4.0 ~/okome/.config/gtk-4.0
-rsync -r ~/.config/hypr ~/okome/.config/hypr
-rsync -r ~/.config/kitty ~/okome/.config/kitty
-rsync -r ~/.config/nwg-look ~/okome/.config/nwg-look
-rsync -r ~/.config/rofi ~/okome/.config/rofi
-rsync -r ~/.config/waybar ~/okome/.config/waybar
-rsync -r ~/.config/wlogout ~/okome/.config/wlogout
-rsync -r ~/.config/wofi ~/okome/.config/wofi
+#sync folders 
+rsync -ra ~/.config/fastfetch ~/okome/.config/fastfetch
+rsync -ra ~/.config/gtk-3.0 ~/okome/.config/gtk-3.0
+rsync -ra ~/.config/gtk-4.0 ~/okome/.config/gtk-4.0
+rsync -ra ~/.config/hypr ~/okome/.config/hypr
+rsync -ra ~/.config/kitty ~/okome/.config/kitty
+rsync -ra ~/.config/nwg-look ~/okome/.config/nwg-look
+rsync -ra ~/.config/rofi ~/okome/.config/rofi
+rsync -ra ~/.config/waybar ~/okome/.config/waybar
+rsync -ra ~/.config/wlogout ~/okome/.config/wlogout
+rsync -ra ~/.config/wofi ~/okome/.config/wofi
+
+rsync -ra /usr/share/sddm/themes ~/okome/usr/share/sddm/themes
+rsync -ra /etc/sddm.conf ~/okome/etc/sddm.conf
 
 git add --all &&  git commit -m 'backup $(date)'
